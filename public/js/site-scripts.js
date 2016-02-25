ebb8bf1cd1e62d9660448285565b2d32912683c5
mocxd <mark.adkins.hastings@gmail.com>
2016-02-25 13:34:41 -0500

    rebuild

---
+++ public/js/site-scripts.js
@@ -0,0 +1,8 @@
+// console.log('trying jsonp');
+// $.ajax({
+//     type: "GET",
+//     url: 'http://mrfpinc.org/feed/?_jsonp=?',
+//     dataType: 'jsonp',
+// }).success( function(response){
+//     console.log(JSON.stringify(response));
+// });
